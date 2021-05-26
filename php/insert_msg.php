b844a70cddec1c91e3f603b9d1cbc454238489c8
HongBY <hby033@naver.com>
2021-05-26 11:15:42 +0900

    05/26 반응형 코드 적용 완료/ contact me 등록일에 년도 4자리 출력으로 수정  / 1차 완료

--- php/insert_msg.php
+++ php/insert_msg.php
@@ -2,20 +2,20 @@
 
   include_once "connect.php";
 
- $name = mysqli_real_escape_string($db_conn, $_POST['name']);
- $email = mysqli_real_escape_string($db_conn, $_POST['email']);
- $subject = mysqli_real_escape_string($db_conn, $_POST['subject']);
- $message = mysqli_real_escape_string($db_conn, $_POST['message']);
- $regist = date("y-m-d");
+  $name = mysqli_real_escape_string($db_conn, $_POST['name']);
+  $email = mysqli_real_escape_string($db_conn, $_POST['email']);
+  $subject = mysqli_real_escape_string($db_conn, $_POST['subject']);
+  $message = mysqli_real_escape_string($db_conn, $_POST['message']);
+  $regist = date("Y-m-d");
 
   //echo $name, $email, $subject, $message;
 
-  $sql = "INSERT INTO re_message (RE_name, RE_email, RE_subject, RE_msg, RE_reg) VALUES('$name', '$email', '$subject', '$message', '$regist')";
+  $sql = "INSERT INTO re_message(RE_name, RE_email, RE_subject, RE_msg, RE_reg) VALUES('$name', '$email', '$subject', '$message', '$regist')";
 
   mysqli_query($db_conn, $sql);
 
   echo "
-    <script>
+    <script>  
       alert('메시지가 입력되었습니다.');
       location.href='/mySite/reveal/index.php';
     </script>
